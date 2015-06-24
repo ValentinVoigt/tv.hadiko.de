937549f00751a63264b54c9b04fb92e06bd1a1ce
Valentin Voigt <mail@valentinvoigt.info>
2015-06-24 03:54:24 +0200

    Switched to outerjoin to display channels without program info

--- tvhadikode/views/epg.py
+++ tvhadikode/views/epg.py
@@ -4,6 +4,8 @@ from datetime import datetime
 
 from pyramid.view import view_config
 from pyramid.renderers import render
+
+from sqlalchemy import and_
 from sqlalchemy.orm import aliased
 
 from tvhadikode.models import DBSession, Service, Program
@@ -11,34 +13,27 @@ from tvhadikode.views.base import BaseView
 
 class HomeViews(BaseView):
 
-    @view_config(route_name='home', renderer='tvhadikode:templates/epg.mak')
-    def epg(self):
+    def build_query(self):
         now = datetime.now()
         current = aliased(Program)
         next_ = aliased(Program)
+
         services = DBSession.query(Service, current, next_)
-        services = services.filter(current.sid==Service.sid, current.start<=now, current.end>=now)
-        services = services.filter(next_.id==current.next_program_id)
-        services = services.order_by(Service.name).all()
-        return {'services': services}
+        services = services.outerjoin(current, and_(current.sid==Service.sid, current.start<=now, current.end>=now))
+        services = services.outerjoin(next_, and_(next_.id==current.next_program_id, current.start<=now, current.end>=now))
+        return services.order_by(Service.name)
+
+    @view_config(route_name='home', renderer='tvhadikode:templates/epg.mak')
+    def epg(self):
+        return {'services': self.build_query().all()}
 
     @view_config(route_name='ajax.epg_update', renderer="json")#, request_method="POST")
     def epg_update(self):
-        # parse request
         slugs = self.request.POST.getall('services[]')
         if len(slugs) == 0:
             return {'rows': {}}
 
-        # build query
-        now = datetime.now()
-        current = aliased(Program)
-        next_ = aliased(Program)
-        service = DBSession.query(Service, current, next_)
-        service = service.filter(Service.slug.in_(slugs))
-        service = service.filter(current.sid==Service.sid, current.start<=now, current.end>=now)
-        service = service.filter(next_.id==current.next_program_id)
-
-        # return result
+        service = self.build_query().filter(Service.slug.in_(slugs))
         rows = {}
         for result in service.order_by(Service.name).all():
             service, current_program, next_program = result
